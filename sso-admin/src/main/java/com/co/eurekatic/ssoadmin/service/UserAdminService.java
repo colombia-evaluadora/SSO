0828ba21c6413a86608251b999b4c148198db4c6
cristian gonzalez franco <97616425+cristian10gf@users.noreply.github.com>
2026-08-28 10:57:43 -0500

    Remove unused UUID import from UserAdminService
    Removed unused import statement for UUID.

--- sso-admin/src/main/java/com/co/eurekatic/ssoadmin/service/UserAdminService.java
+++ sso-admin/src/main/java/com/co/eurekatic/ssoadmin/service/UserAdminService.java
@@ -37,7 +37,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
